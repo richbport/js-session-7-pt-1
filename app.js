35a1f19262e57ad43d5a16d08343a921b6549c32
Rich <richbport@yahoo.com>
2025-08-26 10:45:42 -0400

    nested objects

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-const toyBox = [
-  ["Car", "Doll"],
-  ["Ball", "Puzzle"],
-  ["Yo-Yo", ["Action Figure", "Blocks"]],
-];
+const toyBox = {
+  smallBox: { toys: "Car" },
+  mediumBox: { toys: "Doll" },
+  bigBox: { toys: ["Ball", "Puzzle"] },
+};
 
-for (let list of toyBox) {
-  for (let toy of list) {
-    if(Array.isArray(toy)) {
-      for (let insideToy of toy) {
-        console.log(insideToy)
-      }
-    } else {
-      console.log(toy)
-    }
-  }
-}
+// const toyBox = [
+//   ["Car", "Doll"],
+//   ["Ball", "Puzzle"],
+//   ["Yo-Yo", ["Action Figure", "Blocks"]],
+// ];
+
+// for (let list of toyBox) {
+//   for (let toy of list) {
+//     if(Array.isArray(toy)) {
+//       for (let insideToy of toy) {
+//         console.log(insideToy)
+//       }
+//     } else {
+//       console.log(toy)
+//     }
+//   }
+// }
 
 // 1|1 -> ["Car", "Doll"] === list toy === "Car"
 // 1|2 -> ["Car", "Doll"] === list toy === "Doll"
@@ -24,8 +30,7 @@ for (let list of toyBox) {
 // 3|2 -> ["Yo-Yo", ["Action Figure", "Blocks"]] === list toy === ["Action Figure", "Blocks"]
 // 3|2|1 -> ["Action Figure", "Blocks"] === toy insideToy === "Action Figure"
 // 3|2|2 -> ["Action Figure", "Blocks"] === toy insideToy === "Blocks"
-// 
-
+//
 
 // const toyBox = [
 //   ["Car", "Doll"],
